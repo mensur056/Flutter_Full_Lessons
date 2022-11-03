f396b5477adb04419cff36b0d2223daf8d7a65e4
mensur056 <81297953+mensur056@users.noreply.github.com>
2022-11-03 23:23:05 +0400

    test

--- day_1/lib/second_section/Api/general_dio.dart
+++ day_1/lib/second_section/Api/general_dio.dart
@@ -1,9 +1,11 @@
+import 'package:day_1/second_section/Api/get_request_model.dart';
 import 'package:day_1/second_section/Api/my_model.dart';
 import 'package:dio/dio.dart';
 
 abstract class MyApiService {
   Future<List<UserModel>?> fetchItemFromApi();
   Future<void> userLogin(UserModel model);
+  Future<List<Data>?> fetchFromApiWithData();
 }
 
 class GeneralDio implements MyApiService {
@@ -27,5 +29,16 @@ class GeneralDio implements MyApiService {
     if (response.statusCode == 200) {
       return response.data;
     }
+  }
+
+  @override
+  Future<List<Data>?> fetchFromApiWithData() async {
+    final response = await dio.get('https://reqres.in/api/unknown');
+    if (response.statusCode == 200) {
+      final value = GetRequestModel.fromJson(response.data);
+      return value.data;
+    }
+
+    return null;
   }
 }
---
+++ day_1/lib/second_section/Api/get_request_view.dart
@@ -0,0 +1,54 @@
+import 'package:day_1/second_section/Api/dio_get_learn_page.dart';
+import 'package:day_1/second_section/Api/general_dio.dart';
+import 'package:day_1/second_section/Api/get_request_model.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class GetRequestView extends StatefulWidget {
+  const GetRequestView({Key? key}) : super(key: key);
+
+  @override
+  State<GetRequestView> createState() => _GetRequestViewState();
+}
+
+class _GetRequestViewState extends ProjectLoading<GetRequestView> {
+  List<Data>? items;
+  Dio dio = Dio();
+  late final MyApiService myApiService;
+  @override
+  void initState() {
+    super.initState();
+    myApiService = GeneralDio();
+    fectFromItem();
+  }
+
+  // Future<GetRequestModel?> fetchItemFromApi() async {
+  //   changeWaitValue();
+  //   final response = await dio.get('https://reqres.in/api/unknown');
+  //   if (response.statusCode == 200) {
+  //     final value = GetRequestModel.fromJson(response.data);
+  //     items = value.data;
+  //   }
+  //   changeWaitValue();
+  //   return null;
+  // }
+
+  Future<void> fectFromItem() async {
+    changeWaitValue();
+    items = await myApiService.fetchFromApiWithData();
+    changeWaitValue();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemCount: items?.length ?? 0,
+        itemBuilder: (BuildContext context, int index) {
+          return Text(items?[index].pantoneValue ?? '');
+        },
+      ),
+    );
+  }
+}
